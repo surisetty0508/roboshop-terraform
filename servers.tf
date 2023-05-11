35d7748c7f7835fcc13f43b547dc6114097e5937
prasad <ashoksurisetty0508@gmail.com>
2023-05-11 17:13:24 +0530

    learn terraform

--- servers.tf
+++ servers.tf
@@ -61,13 +61,13 @@ variable "components" {
   }
 }
 resource "aws_instance" "instance" {
-  count                  = length(var.components)
+  for_each               = var.components
   ami                    = data.aws_ami.centos.image_id
-  instance_type          = var.instance_type
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
-    Name = var.components[count.index]
+    Name = each.value["name"]
   }
 }
 
