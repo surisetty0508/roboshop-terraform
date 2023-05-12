f917306269bba1d70f50d1ecbfdf9d2887f0ab63
prasad <ashoksurisetty0508@gmail.com>
2023-05-12 16:01:00 +0530

    learn terraform

--- servers.tf
+++ servers.tf
@@ -1,64 +1,9 @@
-data "aws_ami" "centos" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-}
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-}
-
-
 output "ami" {
   value = data.aws_ami.centos.image_id
 }
 
 variable "instance_type" {
   default ="t3.small"
-}
-
-variable "components" {
-  default = {
-    frontend = {
-      name          = "frontend"
-      instance_type = "t3.small"
-    }
-    mongodb = {
-      name          = "mongodb"
-      instance_type = "t3.small"
-    }
-    catalogue = {
-      name          = "catalogue"
-      instance_type = "t3.small"
-    }
-    user = {
-      name          = "user"
-      instance_type = "t3.small"
-    }
-    rabbitmq = {
-      name          = "rabbitmq"
-      instance_type = "t3.small"
-    }
-    cart = {
-      name          = "cart"
-      instance_type = "t3.small"
-    }
-    redis = {
-      name          = "redis"
-      instance_type = "t3.small"
-    }
-    mysql = {
-      name          = "mysql"
-      instance_type = "t3.small"
-    }
-    payment = {
-      name          = "payment"
-      instance_type = "t3.small"
-    }
-    dispatch = {
-      name          = "dispatch"
-      instance_type = "t3.small"
-    }
-  }
 }
 resource "aws_instance" "instance" {
   for_each               = var.components
